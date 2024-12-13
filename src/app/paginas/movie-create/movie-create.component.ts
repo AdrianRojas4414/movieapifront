4219fb23d65f5a1a89029a1f773afc57fe9a0569
adrian <adrian.rojas.o@gmail.com>
2024-12-13 10:43:08 -0400

    componentes y servicio creado

---
+++ src/app/paginas/movie-create/movie-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-create',
+  imports: [],
+  templateUrl: './movie-create.component.html',
+  styleUrl: './movie-create.component.scss'
+})
+export class MovieCreateComponent {
+
+}
